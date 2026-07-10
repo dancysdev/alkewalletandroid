cb4a178441b4fa0902976d5752e83ef4bf25e2e6
Dangelo <danglosv@gmail.com>
2026-07-10 13:12:13 -0400

    Creación pantalla Login e implementación de navegación

--- app/src/main/java/com/example/alkewallet/SplashActivity.kt
+++ app/src/main/java/com/example/alkewallet/SplashActivity.kt
@@ -1,6 +1,9 @@
 package com.example.alkewallet
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +12,14 @@ import androidx.core.view.WindowInsetsCompat
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
 
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this, LoginSignupActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 2000)
     }
 }
