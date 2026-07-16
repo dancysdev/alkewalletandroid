190554dd707fc6483ca34c72cb2c7f598f22b56c
Dangelo <danglosv@gmail.com>
2026-07-15 23:27:39 -0400

    Implementación de navegación entre pantallas

--- app/src/main/java/com/example/alkewallet/SignUpActivity.kt
+++ app/src/main/java/com/example/alkewallet/SignUpActivity.kt
@@ -1,4 +1,31 @@
 package com.example.alkewallet
 
-class SignUpActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        // Referencias
+        val btnMakeAccount = findViewById<Button>(R.id.btnMakeAccount)
+        val tvYaTieneCuenta = findViewById<TextView>(R.id.tvYaTieneCuenta)
+
+        // Crear cuenta → Home (decidirá aleatoriamente qué layout mostrar)
+        btnMakeAccount.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }
+
+        // Ya tiene cuenta → Login
+        tvYaTieneCuenta.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+    }
 }
